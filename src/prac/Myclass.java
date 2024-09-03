f454ce46f981d4ba99602ea74fc0049bfe40e05a
manishabhongal <bhongalmanisha@gmail.com>
2024-09-03 10:17:07 +0530

    changes in new branch

--- src/prac/Myclass.java
+++ src/prac/Myclass.java
@@ -5,7 +5,7 @@ public class Myclass {
 	public static void main(String[] args) {
 		System.out.println("hi");
 		System.out.println(" this is master ");
-
+//new changes done in pullDemoBranch....this is the change
 	
 
 	}
