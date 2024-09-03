65fd029e76baed71246c3f2ef5ea3f34370394b7
manishabhongal <bhongalmanisha@gmail.com>
2024-09-03 09:26:37 +0530

    changes done in master

--- src/prac/Myclass.java
+++ src/prac/Myclass.java
@@ -4,6 +4,6 @@ public class Myclass {
 
 	public static void main(String[] args) {
 		System.out.println("hi");
-		System.out.println(" this is new branch edit..");
+		System.out.println(" this is master branch");
 	}
 }
