6084edcca68775bfde6c5047df678290e54e0e28
manishabhongal <bhongalmanisha@gmail.com>
2024-09-03 10:13:21 +0530

    new edit

--- src/prac/Myclass.java
+++ src/prac/Myclass.java
@@ -5,7 +5,8 @@ public class Myclass {
 	public static void main(String[] args) {
 		System.out.println("hi");
 		System.out.println(" this is master ");
-
+		System.out.println("added new changes in new branch");
+//new edit
 	
 
 	}
