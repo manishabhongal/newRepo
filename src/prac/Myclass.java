2b7e7071ad017267e69bc958b1b0ed09b98b2b60
manishabhongal <bhongalmanisha@gmail.com>
2024-09-03 09:37:00 +0530

    edit done in master before merging

--- src/prac/Myclass.java
+++ src/prac/Myclass.java
@@ -5,5 +5,8 @@ public class Myclass {
 	public static void main(String[] args) {
 		System.out.println("hi");
 		System.out.println(" this is master branch");
+		System.out.println("master edit 1");
+		System.out.println("master edit 2");
+		//edit 3
 	}
 }
