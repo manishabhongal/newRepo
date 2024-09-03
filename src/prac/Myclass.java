ae7e4295fc267066c1d909ac0bf22f5294085f9e
manishabhongal <bhongalmanisha@gmail.com>
2024-09-03 09:30:04 +0530

    changes done in demo branch

--- src/prac/Myclass.java
+++ src/prac/Myclass.java
@@ -5,5 +5,7 @@ public class Myclass {
 	public static void main(String[] args) {
 		System.out.println("hi");
 		System.out.println(" this is master branch");
+		System.out.println("demo branch changes 1");
+		System.out.println("demo bfranch changes 2");
 	}
 }
